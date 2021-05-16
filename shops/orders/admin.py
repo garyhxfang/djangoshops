ea84884007dbb53276cf5a9270dd0ee461567892
garyhxfang <84261039+garyhxfang@users.noreply.github.com>
2021-05-16 14:10:40 +0800

    Delete shops directory

--- shops/orders/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from orders.models import Order
-from orders.models import OrderProduct
-
-admin.site.register(Order)
-admin.site.register(OrderProduct)
-# Register your models here.
